4b3ef7f192d072ec14b8cde21422c142b9063940
Besute_ <Boris200723@gmail.com>
2025-06-10 13:30:23 +0300

    succeeded setup and made start

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import './../css/style.css'
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="header__back">
+                <h1 className="header__text">Crafting Worlds, Igniting Adventures</h1>
+                <p className="header__p">We design immersive games that captivate players and push creative boundaries.</p>
+                <div className="button">
+                    <p className="button__text">Read about us</p>
+                </div>
+            </div>
+            <div className="header__circle header__circle_big_pink header__circle_big"></div>
+            <div className="header__circle header__circle_small_pink header__circle_small"></div>
+            <div className="header__circle header__circle_big_cyan header__circle_big"></div>
+            <div className="header__circle header__circle_small_cyan header__circle_small_cyan_1 header__circle_small"></div>
+            <div className="header__circle header__circle_small_cyan header__circle_small_cyan_2 header__circle_small"></div>
+            <div className="header__circle header__circle_big_blue header__circle_big"></div>
+            <div className="header__circle header__circle_small_blue header__circle_small"></div>
+        </div>
+    )
+}
+
+export default Header
